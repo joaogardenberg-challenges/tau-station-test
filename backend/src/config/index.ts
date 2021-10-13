dfa47bb367d9ca81adb3ab24105da4b2239e298f
Johnny <joaogardenberg@gmail.com>
2021-10-13 16:07:59 +0200

    Move type to types file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,11 +1,10 @@
 import isNil from 'lodash/isNil'
+import { Config } from '../types'
 
 export const NODE_ENV = process.env.NODE_ENV
 export const PORT = process.env.PORT
 
-const config: {
-  [key: string]: string | number | boolean | undefined
-} = { NODE_ENV, PORT }
+const config: Config = { NODE_ENV, PORT }
 
 Object.keys(config).forEach((key: string) => {
   if (isNil(config[key])) {
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -2,3 +2,7 @@ export interface Image {
   id: number
   url: string
 }
+
+export interface Config {
+  [key: string]: string | number | boolean | undefined
+}
