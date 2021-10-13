b1927f1a45c4181fe2d3d2265ed568fba4189249
Johnny <joaogardenberg@gmail.com>
2021-10-13 18:31:32 +0200

    Created isFetchingImage selector

--- frontend/src/selectors/images.ts
+++ frontend/src/selectors/images.ts
@@ -2,10 +2,13 @@ import { StoreState } from 'types'
 
 export const getImages = (state: StoreState) => state.images
 
-export const isFethchingImages = (state: StoreState) => state.images.isFetching
+export const isFetchingImages = (state: StoreState) => state.images.isFetching
 
 export const isWatchingImages = (state: StoreState) => state.images.isWatching
 
 export const getImagesList = (state: StoreState) => state.images.list
 
 export const getImage = (state: StoreState, id: number) => state.images.list[id]
+
+export const isFetchingImage = (state: StoreState, id: number) =>
+  Boolean(state.images.list[id]?.isFetching)
