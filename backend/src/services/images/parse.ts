ab94d5ef075b2052b99d7bde2195f333ed955a23
Johnny <joaogardenberg@gmail.com>
2021-10-13 10:35:01 +0200

    Use default size const

--- backend/src/services/images/parse.ts
+++ backend/src/services/images/parse.ts
@@ -1,9 +1,11 @@
 import { Image } from '../../interfaces'
 
+export const DEFAULT_SIZE = 200
+
 export default function parseImage(
   image: Image,
-  width: number | string = 200,
-  height: number | string = 200
+  width: number | string = DEFAULT_SIZE,
+  height: number | string = DEFAULT_SIZE
 ) {
   return { ...image, url: `${image.url}/${width}/${height}` }
 }
