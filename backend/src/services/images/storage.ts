90d1e231c3a01aaf0e5948b4e27a92ab5f97b27f
Johnny <joaogardenberg@gmail.com>
2021-10-13 10:35:24 +0200

    Don't copy the memory reference

--- backend/src/services/images/storage.ts
+++ backend/src/services/images/storage.ts
@@ -7,7 +7,7 @@ export const INITIAL_IMAGE_COUNT = 100
 const INITIAL_IMAGES = range(INITIAL_IMAGE_COUNT).map((i) => createImage(i + 1))
 
 export default function imagesStorage() {
-  const images: Image[] = INITIAL_IMAGES
+  const images: Image[] = [...INITIAL_IMAGES]
 
   const getImage = (id: number) => images.find((image) => image.id === id)
 
