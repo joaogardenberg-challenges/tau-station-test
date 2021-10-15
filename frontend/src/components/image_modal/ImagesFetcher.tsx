ffd92c6036bd6efc5626e2208c97eab04778ae6b
Johnny <joaogardenberg@gmail.com>
2021-10-15 03:44:07 +0200

    Only fetch if id > 1

--- frontend/src/components/image_modal/ImagesFetcher.tsx
+++ frontend/src/components/image_modal/ImagesFetcher.tsx
@@ -22,7 +22,12 @@ export default function ImagesFetcher() {
   const firstImageId = Math.max(...imagesIds)
 
   useEffect(() => {
-    if (id && lastImageId && Object.keys(images).length && id <= lastImageId) {
+    if (
+      id > 1 &&
+      lastImageId &&
+      Object.keys(images).length &&
+      id <= lastImageId
+    ) {
       dispatch(
         fetchImages({ fromId: lastImageId - 1, limit: lastImageId - id })
       )
