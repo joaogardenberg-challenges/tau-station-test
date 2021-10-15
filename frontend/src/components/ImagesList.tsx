63e5aa9b222c72c16564bdb14c3c433e37da2961
Johnny <joaogardenberg@gmail.com>
2021-10-15 21:47:17 +0200

    Remove unused var

--- frontend/src/components/ImagesList.tsx
+++ frontend/src/components/ImagesList.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { fetchImages } from 'actions'
 
