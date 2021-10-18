0bc567ddeb52b86fa7d2cf0e88b6a01f71deaa20
Johnny <joaogardenberg@gmail.com>
2021-10-18 19:20:38 +0200

    Created ImageFiller component

---
+++ frontend/src/components/images_list/ImageFiller.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const StyledImageFiller = styled.li<ImageFillerProps>`
+  height: calc(
+    (100vh - ${({ theme, perColumn }) => theme.spacing(perColumn * 2 + 2)}) /
+      ${({ perColumn }) => perColumn}
+  );
+  list-style: none;
+  margin: ${({ theme }) => theme.spacing(1)};
+  width: calc(
+    (100vw - ${({ theme, perRow }) => theme.spacing(perRow * 2 + 2)}) /
+      ${({ perRow }) => perRow}
+  );
+`
+
+interface ImageFillerProps {
+  perRow: number
+  perColumn: number
+}
+
+export default function ImageFiller({ perRow, perColumn }: ImageFillerProps) {
+  return <StyledImageFiller perRow={perRow} perColumn={perColumn} />
+}
