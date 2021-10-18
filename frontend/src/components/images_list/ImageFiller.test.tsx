f3941b4c9c11450cb033aedfa7a7806b40e497de
Johnny <joaogardenberg@gmail.com>
2021-10-18 23:56:21 +0200

    Remove unused var

--- frontend/src/components/images_list/ImageFiller.test.tsx
+++ frontend/src/components/images_list/ImageFiller.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import ThemeProvider from 'providers/Theme'
 
 const ImageFiller = require('./ImageFiller').default
