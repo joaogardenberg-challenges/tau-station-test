c47efe7b34b3add32b668f394fa2ce6f75a2f242
Johnny <joaogardenberg@gmail.com>
2021-10-18 19:21:33 +0200

    Created ImageCard component

---
+++ frontend/src/components/images_list/ImageCard.styled.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components'
+
+interface StyledImageCardProps {
+  perRow: number
+  perColumn: number
+  isVisible: boolean
+}
+
+export default styled.li<StyledImageCardProps>`
+  list-style: none;
+  margin: ${({ theme }) => theme.spacing(1)};
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
+  &.transition-appear,
+  &.transition-enter {
+    & > .content {
+      opacity: 0;
+      transform: scale(0);
+    }
+    &[class$='active'] {
+      & > .content {
+        opacity: 1;
+        transform: none;
+        transition: all ${({ theme }) => theme.transitions.duration.standard}ms;
+      }
+    }
+  }
+  & > .content {
+    align-items: center;
+    background-color: #eee;
+    border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+    display: flex;
+    height: calc(
+      (100vh - ${({ theme, perColumn }) => theme.spacing(perColumn * 2 + 2)}) /
+        ${({ perColumn }) => perColumn}
+    );
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+    width: calc(
+      (100vw - ${({ theme, perRow }) => theme.spacing(perRow * 2 + 2)}) /
+        ${({ perRow }) => perRow}
+    );
+    &:hover > .overlay {
+      opacity: 1;
+    }
+    & > img {
+      max-height: 100%;
+      max-width: 100%;
+      &.loading,
+      &.errored {
+        display: none;
+      }
+    }
+    & > .overlay {
+      background-color: rgba(0, 0, 0, 0.3);
+      display: flex;
+      flex-flow: row wrap;
+      height: 100%;
+      left: 0;
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      transition: opacity
+        ${({ theme }) => theme.transitions.duration.standard}ms;
+      width: 100%;
+      & > .MuiButton-root {
+        border-radius: 0;
+        flex: 50%;
+        text-shadow: 1px 1px #666;
+        &:hover {
+          background-color: rgba(255, 255, 255, 0.2);
+        }
+      }
+    }
+    & > .error {
+      font-size: 5rem;
+    }
+  }
+`
---
+++ frontend/src/components/images_list/ImageCard.tsx
@@ -0,0 +1,80 @@
+import { useRef, useContext, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+import Button from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress'
+import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported'
+import classNames from 'classnames'
+import { ThemeContext } from 'styled-components'
+import { getImage } from 'selectors'
+import { StoreState } from 'types'
+import { WIDTHS } from 'config/constants'
+import useOnScreen from 'hooks/useOnScreen'
+import StyledImageCard from './ImageCard.styled'
+
+interface ImageCardProps {
+  imageId: number
+  perRow: number
+  perColumn: number
+}
+
+export default function ImageCard({
+  imageId,
+  perRow,
+  perColumn
+}: ImageCardProps) {
+  const theme = useContext(ThemeContext)
+  const image = useSelector((s: StoreState) => getImage(s, imageId))
+  const ref = useRef()
+  const isVisible = useOnScreen(ref)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [errored, setErrored] = useState<boolean>(false)
+  const { urls, meta } = image
+  const src = urls?.[400]
+
+  const renderLink = (width: number) => (
+    <Button
+      key={width}
+      color="secondary"
+      component={Link}
+      to={`/images/${imageId}?width=${width}`}
+    >
+      {width}px
+    </Button>
+  )
+
+  return (
+    <CSSTransition
+      in={isVisible}
+      appear
+      classNames="transition"
+      timeout={theme.transitions.duration.standard}
+    >
+      <StyledImageCard
+        perRow={perRow}
+        perColumn={perColumn}
+        ref={ref as any}
+        isVisible={isVisible}
+      >
+        <div className="content">
+          <img
+            className={classNames({ loading, errored })}
+            src={src}
+            alt={meta?.keywords}
+            onLoad={() => setLoading(false)}
+            onError={() => setErrored(true)}
+          />
+          {errored ? (
+            <ImageNotSupportedIcon className="error" color="primary" />
+          ) : (
+            loading && (
+              <CircularProgress color="primary" size={35} thickness={4} />
+            )
+          )}
+          <div className="overlay">{WIDTHS.map(renderLink)}</div>
+        </div>
+      </StyledImageCard>
+    </CSSTransition>
+  )
+}
