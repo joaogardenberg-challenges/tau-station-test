b483217924b80aa8a32df4a536149a54627240a1
Johnny <joaogardenberg@gmail.com>
2021-10-19 03:59:09 +0200

    Enhanced useQuery test

--- frontend/src/hooks/useQuery.test.tsx
+++ frontend/src/hooks/useQuery.test.tsx
@@ -1,10 +1,21 @@
-import '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 const useLocation = jest.fn()
 
 jest.doMock('react-router-dom', () => ({ useLocation }))
 
 const useQuery = require('./useQuery').default
+
+const Component = () => {
+  const query = useQuery()
+
+  return (
+    <>
+      <p>{query.get('width')}</p>
+      <p>{query.toString()}</p>
+    </>
+  )
+}
 
 describe('useQuery', () => {
   const width = 400
@@ -14,9 +25,14 @@ describe('useQuery', () => {
     useLocation.mockReturnValue({ search })
   })
 
-  it('returns an instance of URLSearchParams that functions properly', () => {
-    const query = useQuery()
-    expect(query.toString()).toEqual(search.substring(1))
-    expect(query.get('width')).toEqual(width.toString())
+  it('functions properly in a component', () => {
+    const { container } = render(<Component />)
+
+    const widthText = screen.getByText(width.toString())
+    const queryText = screen.getByText(search.substring(1))
+
+    expect(widthText).toBeInTheDocument()
+    expect(queryText).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
   })
 })
